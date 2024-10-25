903719c6de76a8b9cc82bb06900bec1f18a4daef
Azriel Amiezal <amiezalazwary@gmail.com>
2024-10-25 16:48:33 +0800

    requirement text

--- Q1.py
+++ Q1.py
@@ -1,9 +1,5 @@
 #import libraries
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib as plt
-import plotly.express as px
 
 #import data
 df = pd.read_csv("dataset.csv")
