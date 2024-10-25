299d24b868e83785ad7ca31de5883beabcb2aa1e
AlifSafwan01 <muhammadalifsaf@gmail.com>
2024-10-25 10:07:00 +0800

    Ini file

---
+++ savedfile.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as plt
+import plotly.express as px
+
+#import data
+df = pd.read_csv("dataset.csv")
+
+#df info
+print(df.info())
